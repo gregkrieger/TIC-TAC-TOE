af6c882d311e0e1663b3e94c932e8d3bcda341e1
Greg <Gregory.krieger94@gmail.com>
2022-05-31 19:04:05 -0400

    Setting up Bootstrap

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GameBoard = ((x, o) => {
+const GameBoard = ((move) => {
     /**Create the gameboard initially with Jquery, 
      * loop over the gameboard and push it into 
      * myGameBoard. Use Jquery to update array
@@ -38,9 +38,20 @@ const GameBoard = ((x, o) => {
 })();
 GameBoard.appendTable;
 const displayController = (() =>{
-    
+  /**Use Jquery to create a controller
+   * These controllers will be returned
+   * Each player will have their own controller 
+   * Controller will write to array by passing 
+   * the move to the gameBoard. 
+   * Will have a scoreboard function.
+   */
+    let control = ``
 });
 
 const player = () => {
-
+  /**function factory to create players 
+   * on inception of window and DOM each
+   * player will be give a controller 
+   * Each player will be given a scoreboard
+   */
 };
